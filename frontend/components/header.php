6edd35fd18b110e1bc5bfea37716717374ad5b05
Nathan M <nathanbmiranda@hotmail.com>
2023-01-26 09:42:10 -0300

    code

--- frontend/components/header.php
+++ frontend/components/header.php
@@ -1,22 +1,38 @@
 <?php
 if ($_SESSION['logged']) {
+    // $_SESSION['btn'] = "<button class='nav-item btn btn-link' type='submit' name='logout'
+    // style='margin-left:-14px; color: orange;'>Logout</button>";
     $_SESSION['btn'] = "<button class='nav-item btn btn-link' type='submit' name='logout'
-    style='margin-left:-14px; color: orange;'>Logout</button>";
+    style='margin-left:-14px text-; color: orange; text-decoration: none;'>Exit
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='19' fill='currentColor' class='bi bi-box-arrow-left' viewBox='0 0 16 16'>
+      <path fill-rule='evenodd' d='M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z'/>
+      <path fill-rule='evenodd' d='M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z'/>
+    </svg> </button>";
+    
+//     <button class='nav-item btn btn-link' type='submit' name='logout'
+//     style='margin-left:-14px; color: orange;'>
+//     <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-box-arrow-in-right' viewBox='0 0 16 16'>
+//     <path fill-rule='evenodd' d='M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z'/>
+//     <path fill-rule='evenodd' d='M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z'/>
+//   </svg></button>"
+
 }else {
-    $_SESSION['btn'] = "<a href='/frontend/page/login' class='nav-item'>Login</a>";
+     $_SESSION['btn'] = "<a href='/frontend/page/login' class='nav-item'>
+     <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-box-arrow-in-right' viewBox='0 0 16 16'>
+        <path fill-rule='evenodd' d='M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z'/>
+        <path fill-rule='evenodd' d='M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z'/>
+    </svg> Login</a>";
+    // $_SESSION['btn'] = "<button class='nav-item btn btn-link' href='/frontend/page/login'
+    // >Login</button>";
 }
 ?>
 <header id="nav">
     
-    <nav class="navbar fixed-top " style="background-color: #141414;" id="navbar">
+    <nav class="navbar navbar-expand-lg navbar sticky-top " style="background-color: #141414;" id="navbar">
         <div class="container">
             <a class="navbar-brand" href="/index">
-                <img src="/assets/imgs/library.png" style="margin-bottom: 11px;" alt="Bootstrap" width="30" height="25" style="margin">
-                <span id="orange-text">Biblioteca</span>
+                <img src="/assets/imgs/library.png" style="margin-bottom: 11px; margin-right: .8em;" alt="Bootstrap" width="30" height="25">
             </a>
-            <a class="navbar-brand" href="/frontend/page/livros"><span id="orange-text">Livros</span></a>
-            <a class="navbar-brand" href="/frontend/page/cadastros?page=1"><span id="orange-text">Cadastros</span></a>
-            <!-- <a class="navbar-brand" href="#"><span id="orange-text">asd</span></a> -->
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" style="border: none;" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
                 <span id="navicon"><img src="/assets/imgs/menu-4-32.ico" alt=""></span>
             </button>
@@ -26,10 +42,31 @@ if ($_SESSION['logged']) {
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/frontend/page/writerForm?page=1"><span id="orange-text">Escritores</span></a>
+                            <a class="navbar-brand" href="/index">
+                                Biblioteca
+                            </a>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/frontend/page/livros"
+                            style="margin-right: 1em;">Livros</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/frontend/page/cadastros?page=1"
+                            style="margin-right: 1em;">Cadastros</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/frontend/page/writerForm?page=1"
+                            style="margin-right: 1em;">Escritores</a>
+                        </li>
+                    </ul>
+                    <!-- Login/Logout -->
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+
                         <li class="nav-item">
                             <form action="/backend/operations/userOperation.php" method="post">
                             <?php
@@ -45,4 +82,3 @@ if ($_SESSION['logged']) {
             </div>
         </div>
     </nav>
-</header>
